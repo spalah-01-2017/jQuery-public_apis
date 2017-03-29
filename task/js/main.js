f9b9deb5bd6ddffbaab4c118ff0006f37c8a268f
Kuznetsova-Anna <kuznetsova.nura@gmail.com>
2017-03-29 23:23:27 +0300

    partially done (link for the city)

--- task/js/main.js
+++ task/js/main.js
@@ -1,1 +1,24 @@
 
+
+
+// var city = $('#city');
+var city = 'Москва';
+
+
+$.ajax({
+    method: "GET",
+    url: "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + city + "&format=json&callback=wikiCallback",
+    // context: document.body,
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+        format: "json"
+    },
+
+    // Work with the response
+    success: function( response ) {
+        $('#cityLink').attr("href", response[3][0]);
+        console.log( response[3][0]); // server response
+    }
+});
+
