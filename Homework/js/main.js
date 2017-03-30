a0bfa6d75abaa42a786c2a44e0723df8a485a43d
Костя <>
2017-03-30 16:22:24 +0300

    Corrected

--- Homework/js/main.js
+++ Homework/js/main.js
@@ -3,17 +3,28 @@ $('#submit-btn').click(function(e){
 	var nameCity = $('#city').val();
 	var nameStreet =$('#street').val();
 	var url = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+nameCity+'&format=json&callback=wikiCallback'
-	$.ajax({
-		type: "GET",
-		url: url,
-		dataType: "JSONP",
-		cache: false,
-		success: function(data){
-			function addImg(nameStreet,nameCity) {
-			  $('body').append('<img src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+nameStreet+','+nameCity+'" class="bgimg" alt="" />');
+	if(nameCity.trim() != "" && nameStreet.trim() != ""){
+		function addImg(nameStreet,nameCity) {
+			$('img').remove();
+			$('body').append('<img src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+nameStreet+','+nameCity+'" class="bgimg" alt="" />');
+		};
+		addImg(nameStreet,nameCity);
+		$.ajax({
+			type: "GET",
+			url: url,
+			dataType: "JSONP",
+			cache: false
+		})
+		.then(function(data){
+			$('#wikiLinksList').empty();
+			for(var i=0; i<data[data.length-1].length; i++){
+				$('#wikiLinksList').append('<li><a href='+data[data.length-1][i]+'>'+data[1][i]+'</a></li>');
 			}
-			addImg(nameStreet,nameCity);
-			$('#wikiLinksList').html('<li><a href='+data[data.length-1][0]+'>'+data[0]+'</a></li>');
-		}
-	});
+		})
+		.fail(function(){
+			alert('Что-то пошло не так!');
+		})
+	}else{
+		alert("Заполните все поля!")
+	};
 });
